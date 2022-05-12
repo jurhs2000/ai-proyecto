062e8679d17eefa5dca757b81b8b64ec88b2c440
Julio Herrera <42546972+jurhs2000@users.noreply.github.com>
2022-05-12 02:56:02 -0600

    feat; added datasets

---
+++ main.py
@@ -0,0 +1,10 @@
+# Universidad del Valle de Guatemala
+# Inteligencia Artificial - CC3045
+# Authors:
+# Julio Herrera - 19402
+# Juan Pablo Pineda - 19087
+# Diego Crespo - 19541
+
+# Dataset:
+# games_of_all_time.csv - Games of All Time from Metacritic - https://www.kaggle.com/datasets/xcherry/games-of-all-time-from-metacritic?resource=download
+# vgsales.csv - Video Game Sales - https://www.kaggle.com/datasets/gregorut/videogamesales
